f5c78118a5237dd7200b14a9f35fd6f62cd36a69
ViktorZharina <viktorz1986@gmail.com>
2013-08-30 11:47:39 +0700

    refactoring

--- src/banki.py
+++ src/banki.py
@@ -1,65 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import urllib2
-import banks
+from banks import *
 import json
 from bs4 import BeautifulSoup
 
 def main():
-    print "Банк \t\t\t\t\t USDBuy\t\tUSDSell\t\tEURBuy\t\tEURSell"
+    bank_rate = {}
 
-    currency_list = ['usd', 'eur']
-    type_list = ['buy', 'sell']
+    for bank in banks_info:
+        source = get_source(bank['url'], bank['encoding'])
+        info = (bank['name'] + ' ' + bank['address'] + ' ' + bank['phone'] + ' ')
+        bank_rate[info] = get_exchange_rate(source, bank, settings)
 
-    all_prices = {
-        'usd':
-        {
-            'buy': { 'prices': [] },
-            'sell': { 'prices': [] }
-        },
-        'eur':
-        {
-            'buy': { 'prices': [] },
-            'sell': { 'prices': [] }
-        }
-    }
+    table = get_currency_table(bank_rate)
+    print (table)
+    banks = get_banks_for_exchange(bank_rate)
+    print banks
 
-    best_bank_price = {}
-
-    for url in banks.params.keys():
-        print url
-        params = banks.params[url]
-        src = getSource(url, params['encoding'])
-        header = params['name'] + ' ' +  params['address'] + ' ' + params['phone'] + ' '
-        body = ""
-
-        for c in currency_list:
-            for t in type_list:
-                price = getPrice(c, t, src, params)
-                all_prices[c][t]['prices'].append(price)
-                best_bank_price[price] = header
-                body += '\t|' + price + '\t|'
-
-        print (header + body).encode('utf-8')
-        print '==============================================================================================================='
-
-    best_buy_price = getBest('usd', 'buy', all_prices).replace(',', '.')
-    best_sell_price = getBest('usd', 'sell', all_prices).replace(',', '.')
-
-    print best_buy_price, best_sell_price
-
-    if (best_buy_price > best_sell_price):
-        print "RUR na USD " + best_buy_price.encode('utf-8') + " v " + best_bank_price[best_buy_price].encode('utf-8') + " USD na RUR " + best_sell_price.encode('utf-8') + " v " + best_bank_price[best_sell_price].encode('utf-8')
-
-    best_buy_price = getBest('eur', 'buy', all_prices)
-    best_sell_price = getBest('eur', 'sell', all_prices)
-
-    print best_buy_price, best_sell_price
-
-    if (best_buy_price > best_sell_price):
-        print "RUR na EUR v " + best_bank_price[best_buy_price].encode('utf-8') + " EUR na RUR v " + best_bank_price[best_sell_price].encode('utf-8')
-
-def getSource(url, encoding):
+def get_source(url, encoding):
     try:
         fileHandle = urllib2.urlopen(url)
         source = fileHandle.read()
@@ -69,31 +28,89 @@ def getSource(url, encoding):
         source = u'err'
     return source.decode(encoding, 'ignore')
 
-def getBest(cur, ttype, all_prices):
-    if (ttype == 'buy'):
-        return max(all_prices[cur][ttype]['prices'])
-    elif (ttype == 'sell'):
-        return min(all_prices[cur][ttype]['prices'])
-    else:
-        return 0
+def get_currency_table(bank_rate):
+    s = ""
+    table = ""
+    for info in bank_rate.keys():
+        for k,v in bank_rate[info].items():
+            s += "\t " + k + " " + v + " | "
+        table += info + s + '\n' + 150 * "=" + '\n'
+        info = ""
+        s = ""
 
-def getPrice(cur, ttype, src, params):
+    return table.encode('utf-8')
+
+def get_exchange_rate(src, bank_params, settings):
+    exchange_rate = {}
+    for c in settings['currencys']:
+        for op in settings['operations']:
+            exchange_rate[c +"_" + op] = get_currency_value(c, op, src, bank_params)
+    return exchange_rate
+
+def get_currency_value(cur, ttype, src, params):
     if ('method' in params.keys()):
         if (params['method'] == 'soup'):
             soup = BeautifulSoup(src)
-            price = soup.find(params[cur + '_ankor'], {'id':params[cur + '_' + ttype + '_offset']}).get_text(strip=True)
+            currency_value = soup.find(params[cur + '_ankor'], {'id':params[cur + '_' + ttype + '_offset']}).get_text(strip=True)
         elif (params['method'] == 'json'):
             json_data = json.loads(src)
+
             if (ttype == 'sell'):
-                price = json_data['today']['sale'][cur]
+                ##SALE!!!
+                currency_value = json_data['today']['sale'][cur]
             else:
-                price = json_data['today'][ttype][cur]
+                currency_value = json_data['today'][ttype][cur]
+            currency_value = str(currency_value)
     else:
         cur_type_offset = params[cur + '_' + ttype + '_offset']
         ankor_idx = src.find(params[cur + '_ankor'])
         buy_start = ankor_idx + cur_type_offset
-        price = src[buy_start : buy_start + params['offset']]
-    return price.replace(',', '.')
+        currency_value = src[buy_start : buy_start + params['offset']]
+
+    return currency_value.replace(',', '.')
+
+def get_banks_for_exchange(bank_rate):
+    all_values = {}
+    bank = {}
+
+    #get_all_curency_values
+
+    for info in bank_rate:
+        for cur_op_name, val in bank_rate[info].items():
+            if cur_op_name in all_values.keys():
+                all_values[cur_op_name].append(val)
+                bank[val] = info
+            else:
+                all_values[cur_op_name] = [val]
+                bank[val] = info
+
+    best_values = get_best_values(all_values)
+    best_banks = get_banks_by_best_values(best_values, bank)
+    print best_banks
+    return best_banks
+
+def get_best_values(all_values):
+    best_values = {}
+
+    for cur_op, value_list in all_values.items():
+        best_values[cur_op] = max(value_list) if (cur_op[4:] == 'buy') else min(value_list)
+
+    return best_values
+
+def must_exchange(best_banks):
+    best_banks
+
+def get_banks_by_best_values(best_values, bank):
+
+    banks = {'Not at this time' : 'see ya...'}
+
+    if (best_values['usd_buy'] > best_values['usd_sell']):
+        banks['USD GO GO GO !!!'] = {'from': bank[best_values['usd_buy']], 'to': bank[best_values['usd_sell']]}
+
+    if (best_values['eur_buy'] > best_values['eur_sell']):
+        banks['EUR GO GO GO !!!'] = {'from': bank[best_values['eur_buy']], 'to': bank[best_values['eur_sell']]}
+
+    return banks
 
 if __name__ == "__main__":
     main()
--- src/banks.py
+++ src/banks.py
@@ -1,107 +1,118 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-params = {
-'http://www.nskbl.ru':
-{
-	'name'					: u'Левобережный',
-	'address'				: u'Совпаршкольный 13',
-	'phone'					: '900-410',
-	'work_time'				: '09.00-20.00',
-	'usd_ankor'				: u'<td>Доллар США</td>',
-	'usd_buy_offset'                        : 157,
-	'usd_sell_offset'                       : 274,
-	'eur_ankor'				: u'<td>Евро</td>',
-	'eur_buy_offset'                        : 151,
-	'eur_sell_offset'                       : 268,
-	'offset' 				: 5,
-	'encoding'				: 'cp1251'
-},
-'http://novosibirs.pkb.ru/work/informer/currency.asp?87971':
-{
-	'name'					: u'Петрокомерц',
-	'address'				: u'Трифонова,22',
-	'phone'					: '51-27-60',
-	'work_time'				: '10.00 - 17.00',
-	'usd_ankor'				: u'USD',
-	'usd_buy_offset'                        : 44,
-	'usd_sell_offset'                       : 92,
-	'eur_ankor'				: u'EUR',
-	'eur_buy_offset'                        : 44,
-	'eur_sell_offset'                       : 92,
-	'offset' 				: 5,
-	'encoding'				: 'cp1251'
-},
- 'http://www.rshb.ru/filiali/55/currency-rates.php':
- {
- 	'name'					: u'Россельхоз',
- 	'address'				: u'МосковТракт,д.8б',
- 	'phone'					: '20-22-24',
- 	'work_time'				: '09.00-19.30',
- 	'usd_ankor'				: u'Доллар США',
- 	'usd_buy_offset'                        : 24,
- 	'usd_sell_offset'                       : 43,
- 	'eur_ankor'				: u'ЕВРО',
- 	'eur_buy_offset'                        : 18,
- 	'eur_sell_offset'                       : 37,
- 	'offset' 				: 5,
- 	'encoding'				: 'cp1251'
- },
-'http://www.skbbank.ru':
-{
-	'name'					: u'СКБ',
-	'address'				: u'Томск, пр. Ленина,94',
-	'phone'					: '51-18-49',
-	'work_time'				: '09.00-20.00',
-	'usd_ankor'				: u'td',
-        'usd_buy_offset'                        : 'val_0',
-        'usd_sell_offset'                       : 'val_1',
-	'eur_ankor'                         	: u'td',
-        'eur_buy_offset'                        : 'val_3',
-	'eur_sell_offset'                       : 'val_4',
-	'offset' 				: 5,
-	'encoding'				: 'utf-8',
-        'method'                                : 'soup'
-},
-    'http://tomsk.rostbank.ru/offices':
-    {
-	'name'					: u'РОСТ_БАНК',
-	'address'				: u'ул.Дзержинского,54а',
-	'phone'					: '70-50-51',
-	'work_time'				: '09:30 - 19:00',
-	'usd_ankor'				: u'USD',
-	'usd_buy_offset'                        : 12,
-	'usd_sell_offset'                       : 26,
-	'eur_ankor'                         	: u'EUR',
-	'eur_buy_offset'                        : 12,
-	'eur_sell_offset'                       : 26,
-	'offset' 				: 5,
-	'encoding'				: 'utf-8'
-},
-'http://www.bank-kansky.ru':
-{
-	'name'					: u'Канский',
-	'address'				: u'пр. Кирова, 35',
-	'phone'					: '55-50-90',
-	'work_time'				: '10.00 - 19.00',
-	'usd_ankor'				: u'USD',
-	'usd_buy_offset'                        : 39,
-	'usd_sell_offset'                       : 71,
-	'eur_ankor'				: u'EUR',
-	'eur_buy_offset'                        : 39,
-	'eur_sell_offset'                       : 71,
-	'offset' 				: 5,
-	'encoding'				: 'utf-8'
-},
-'https://direct.binbank.ru/api/rate.php?city=Томск&callback=?':
-    {
-	'name'					: u'БинБанк',
-	'address'				: u'Кирова,3Б',
-	'phone'					: '56-40-37',
-	'work_time'				: '9:00-20:00',
-	'usd_ankor'				: u'today',
-	'eur_ankor'				: u'today',
-	'encoding'				: 'utf-8',
-        'method'                                : 'json'
-	}
 
+settings = {
+    'currencys'  : ['usd', 'eur'],
+    'operations' : ['buy', 'sell'],
+    'usd_buy'    : u'USD покупка',
+    'usd_sell'   : u'USD продажа',
+    'eur_buy'    : u'EUR покупка',
+    'eur_sell'   : u'EUR продажа'
 }
+
+
+
+banks_info = [
+    {
+        'url'                   :'http://www.nskbl.ru',
+        'name'					: u'Левобережный',
+        'address'				: u'Совпаршкольный 13',
+        'phone'					: '900-410',
+        'work_time'				: '09.00-20.00',
+        'usd_ankor'				: u'<td>Доллар США</td>',
+        'usd_buy_offset'        : 157,
+        'usd_sell_offset'       : 274,
+        'eur_ankor'				: u'<td>Евро</td>',
+        'eur_buy_offset'        : 151,
+        'eur_sell_offset'       : 268,
+        'offset' 				: 5,
+        'encoding'				: 'cp1251'
+    },
+    {
+        'url'                   :'http://novosibirs.pkb.ru/work/informer/currency.asp?87971',
+        'name'					: u'Петрокомерц',
+        'address'				: u'Трифонова,22',
+        'phone'					: '51-27-60',
+        'work_time'				: '10.00 - 17.00',
+        'usd_ankor'				: u'USD',
+        'usd_buy_offset'        : 44,
+        'usd_sell_offset'       : 92,
+        'eur_ankor'				: u'EUR',
+        'eur_buy_offset'        : 44,
+        'eur_sell_offset'       : 92,
+        'offset' 				: 5,
+        'encoding'				: 'cp1251'
+    },
+    {
+        'url'                   : 'http://www.rshb.ru/filiali/55/currency-rates.php',
+        'name'					: u'Россельхоз',
+        'address'				: u'МосковТракт,д.8б',
+        'phone'					: '20-22-24',
+        'work_time'				: '09.00-19.30',
+        'usd_ankor'				: u'Доллар США',
+        'usd_buy_offset'        : 24,
+        'usd_sell_offset'       : 43,
+        'eur_ankor'				: u'ЕВРО',
+        'eur_buy_offset'        : 18,
+        'eur_sell_offset'       : 37,
+        'offset' 				: 5,
+        'encoding'				: 'cp1251'
+    },
+    {
+        'url'                   : 'http://www.skbbank.ru',
+        'name'					: u'СКБ',
+        'address'				: u'Томск, пр. Ленина,94',
+        'phone'					: '51-18-49',
+        'work_time'				: '09.00-20.00',
+        'usd_ankor'				: u'td',
+        'usd_buy_offset'        : 'val_0',
+        'usd_sell_offset'       : 'val_1',
+        'eur_ankor'             : u'td',
+        'eur_buy_offset'        : 'val_3',
+        'eur_sell_offset'       : 'val_4',
+        'offset' 				: 5,
+        'encoding'				: 'utf-8',
+        'method'                : 'soup'
+    },
+    {
+        'url'                   : 'http://tomsk.rostbank.ru/offices',
+        'name'					: u'РОСТ_БАНК',
+        'address'				: u'ул.Дзержинского,54а',
+        'phone'					: '70-50-51',
+        'work_time'				: '09:30 - 19:00',
+        'usd_ankor'				: u'USD',
+        'usd_buy_offset'        : 12,
+        'usd_sell_offset'       : 26,
+        'eur_ankor'             : u'EUR',
+        'eur_buy_offset'        : 12,
+        'eur_sell_offset'       : 26,
+        'offset' 				: 5,
+        'encoding'				: 'utf-8'
+    },
+    {
+        'url'                   : 'http://www.bank-kansky.ru',
+        'name'					: u'Канский',
+        'address'				: u'пр. Кирова, 35',
+        'phone'					: '55-50-90',
+        'work_time'				: '10.00 - 19.00',
+        'usd_ankor'				: u'USD',
+        'usd_buy_offset'        : 39,
+        'usd_sell_offset'       : 71,
+        'eur_ankor'				: u'EUR',
+        'eur_buy_offset'        : 39,
+        'eur_sell_offset'       : 71,
+        'offset' 				: 5,
+        'encoding'				: 'utf-8'
+    },
+    {
+        'url'                   : 'https://direct.binbank.ru/api/rate.php?city=Томск&callback=?',
+        'name'					: u'БинБанк',
+        'address'				: u'Кирова,3Б',
+        'phone'					: '56-40-37 \t',
+        'work_time'				: '9:00-20:00',
+        'usd_ankor'				: u'today',
+        'eur_ankor'				: u'today',
+        'encoding'				: 'utf-8',
+        'method'                : 'json'
+    }
+]

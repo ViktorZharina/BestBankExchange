40c9cd9fd1d4f752efdf6f26dafa7f1f593160e0
ViktorZharina <viktorz1986@gmail.com>
2013-09-04 16:44:40 +0700

    create templates and move data from tornado_server to template index.html

--- src/client.py
+++ src/client.py
@@ -5,6 +5,7 @@
 
 import tornado.ioloop
 import tornado.web
+import tornado.template
 from config.banks import *
 from config.settings import *
 import json
@@ -21,47 +22,11 @@ class IndexHandler(tornado.web.RequestHandler):
         for x in xrange(len(url_bank_info)):
             ex_rates[x] = json.loads(mc.get(str(x)))
 
-        table = self.get_table(ex_rates)
-        self.write(table)
-
-    def get_table(self, ex_rates):
         bank = BankBase(ex_rates)
-        all_best_values = bank.get_all_best_values()
-
-        table = u"""<table width=100%><tr><td>Банк</td><td colspan=2 align="center">USD</td><td colspan=2 align="center">EUR</td></tr>
-                    <tr><td></td><td>Покупка</td><td>Продажа</td><td>Покупка</td><td>Продажа</td></tr>"""
-
-        for bank_id, v in ex_rates.items():
-            best_values = list(set(v.values()) & set(all_best_values))
-            usd_buy = v['usd_buy']
-            usd_sell = v['usd_sell']
-            eur_buy = v['eur_buy']
-            eur_sell = v['eur_sell']
-
-            table += u'<tbody><tr><td>'+url_bank_info[bank_id]+u'</td>'
-
-            if usd_buy in best_values:
-                table += '<td><b>'+usd_buy+u'</b></td>'
-            else:
-                table += '<td>'+usd_buy+u'</td>'
-
-            if usd_sell in best_values:
-                table += '<td><b>'+usd_sell+u'</b></td>'
-            else:
-                table += '<td>'+usd_sell+u'</td>'
-
-            if eur_buy in best_values:
-                table += '<td><b>'+eur_buy+u'</b></td>'
-            else:
-                table += '<td>'+eur_buy+u'</td>'
-
-            if eur_sell in best_values:
-                table += '<td><b>'+eur_sell+u'</b></td>'
-            else:
-                table += '<td>'+eur_sell+u'</td>'
-
-        table += u'</tbody></table>'
-        return table
+        best_values = bank.get_all_best_values()
+        loader = tornado.template.Loader("templates")
+        template = loader.load("index.html").generate(bank=bank, ex_rates=ex_rates, best_values = best_values, bank_info = url_bank_info)
+        self.write(template)
 
 application = tornado.web.Application([
     (r"/", IndexHandler),
